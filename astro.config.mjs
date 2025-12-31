22dbb3b57f4bb34863b1bf2eaa133e9a9fdddc7d
Telmooo <telmooo.dev@gmail.com>
2025-12-31 04:51:13 +0000

    feat: add first wiki page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,36 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import starlight from '@astrojs/starlight';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [starlight({
+        title: {
+            en: "Wiki | Telmo Baptista",
+            ja: "知識の宝庫 | Telmo Baptista"
+        },
+        description: "Wiki, System Setups, and Guides",
+        logo: {
+            src: "./src/assets/logo.png"
+        },
+        tableOfContents: {
+            minHeadingLevel: 2,
+            maxHeadingLevel: 3,
+        },
+        editLink: {
+            baseUrl: "https://github.com/Telmooo/miyazono/edit/main/",
+        },
+        sidebar: [
+            // Back to homepage
+            { label: "Home", link: "/" },
+            // Wiki entrypoint
+            { label: "Wiki Introduction", link: "/wiki" },
+            { label: "Linux", autogenerate: { directory: "wiki/linux" } }
+        ],
+        social: [
+            { icon: "github", label: "GitHub", href: "https://github.com/Telmooo" }
+        ],
+        favicon: "/logo.png",
+    })]
+});
---
+++ src/content/docs/wiki/index.md
@@ -0,0 +1,6 @@
+---
+title: Welcome to the Wiki
+description: Wiki, System Setups, and Guides
+---
+
+A place where I store guides, how-tos, configurations and other useful stuff for my (future) self.
