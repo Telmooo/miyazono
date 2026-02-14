f516dc75309b62ad0f206ba3512685ed605db85f
Telmooo <telmooo.dev@gmail.com>
2026-02-14 21:45:16 +0000

    style: apply auto formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,43 +18,46 @@ export default defineConfig({
       algorithm: "SHA-512",
     },
   },
-  integrations: [starlight({
-    plugins: [starlightAutoSidebar()],
-    title: {
-      en: "Wiki | Telmo Baptista",
-      ja: "知識の宝庫 | Telmo Baptista",
-    },
-    description: "Wiki, System Setups, and Guides",
-    disable404Route: true,
-    logo: {
-      src: "./src/assets/logo.webp",
-    },
-    tableOfContents: {
-      minHeadingLevel: 2,
-      maxHeadingLevel: 3,
-    },
-    editLink: {
-      baseUrl: "https://github.com/Telmooo/miyazono/edit/main/",
-    },
-    sidebar: [
-      // Back to homepage
-      { label: "Home", link: "/" },
-      // Wiki entrypoint
-      { label: "Wiki Introduction", link: "/wiki/" },
-      {
-        label: "Awesome",
-        autogenerate: { directory: "wiki/awesome" },
+  integrations: [
+    starlight({
+      plugins: [starlightAutoSidebar()],
+      title: {
+        en: "Wiki | Telmo Baptista",
+        ja: "知識の宝庫 | Telmo Baptista",
       },
-      { label: "Linux", autogenerate: { directory: "wiki/linux" } },
-      { label: "Homelab", autogenerate: { directory: "wiki/homelab" } },
-    ],
-    social: [
-      { icon: "github", label: "GitHub", href: "https://github.com/Telmooo" },
-    ],
-    favicon: "/logo.png",
-  }), react({
-    include: ['**/react/*'],
-  })],
+      description: "Wiki, System Setups, and Guides",
+      disable404Route: true,
+      logo: {
+        src: "./src/assets/logo.webp",
+      },
+      tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 3,
+      },
+      editLink: {
+        baseUrl: "https://github.com/Telmooo/miyazono/edit/main/",
+      },
+      sidebar: [
+        // Back to homepage
+        { label: "Home", link: "/" },
+        // Wiki entrypoint
+        { label: "Wiki Introduction", link: "/wiki/" },
+        {
+          label: "Awesome",
+          autogenerate: { directory: "wiki/awesome" },
+        },
+        { label: "Linux", autogenerate: { directory: "wiki/linux" } },
+        { label: "Homelab", autogenerate: { directory: "wiki/homelab" } },
+      ],
+      social: [
+        { icon: "github", label: "GitHub", href: "https://github.com/Telmooo" },
+      ],
+      favicon: "/logo.png",
+    }),
+    react({
+      include: ["**/react/*"],
+    }),
+  ],
   srcDir: "./src",
   publicDir: "./public",
   outDir: "./dist",
